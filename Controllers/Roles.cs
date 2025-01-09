f943b656a05c77b56c9eee93c2f66481481e9838
jumel <alexis.jumel1@gmail.com>
2025-01-09 15:16:29 +0100

    Add dependencies : - Microsoft.EntityFrameworkCore - Microsoft.EntityFrameworkCore.SqlServer - Microsoft.EntityFrameworkCore.Tools

---
+++ Controllers/Roles.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class Roles : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>() { "Admin", "User" };
+        }
+    }
+}
